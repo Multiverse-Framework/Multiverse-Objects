0c733703ace699dd8b729f5aaf114fca06cabbb8
Giang Nguyen <hoanggia@uni-bremen.de>
2023-09-06 16:33:37 +0200

    Muahaha

---
+++ articulated_objects/box/scripts/spawn_milk.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from mujoco_msgs.msg import ObjectStatus, ObjectInfo
+from mujoco_msgs.srv import SpawnObject, SpawnObjectRequest
+from std_srvs.srv import Trigger, TriggerRequest
+
+import rospkg
+
+
+def spawn_box() -> None:
+    rospack = rospkg.RosPack()
+
+    object_status = ObjectStatus()
+    object_status.info.name = "milk_box"
+    object_status.info.type = ObjectInfo.CUBE
+    object_status.info.movable = True
+
+    object_status.info.inertial.m = 0.01
+
+    object_status.info.size.x = 0.03
+    object_status.info.size.y = 0.03
+    object_status.info.size.z = 0.09
+    object_status.info.rgba.a = 1
+    object_status.info.rgba.r = 0
+    object_status.info.rgba.g = 1
+    object_status.info.rgba.b = 0
+
+    object_status.pose.position.x = 2.5
+    object_status.pose.position.y = 2.5
+    object_status.pose.position.z = 1.0
+    
+    object_status.pose.orientation.x = 0.707
+    object_status.pose.orientation.y = 0.0
+    object_status.pose.orientation.z = 0.0
+    object_status.pose.orientation.w = 0.707
+
+    objects = SpawnObjectRequest()
+    objects.objects = [object_status]
+    rospy.wait_for_service("/mujoco/spawn_objects")
+    try:
+        spawn_objects = rospy.ServiceProxy("/mujoco/spawn_objects", SpawnObject)
+        spawn_resp = spawn_objects(objects)
+        rospy.loginfo("Spawn response: " + str(spawn_resp))
+    except rospy.ServiceException as error:
+        print(f"Service call failed: {error}")
+
+
+def reset_simulation():
+    rospy.wait_for_service("/mujoco/reset")
+    try:
+        reset_service = rospy.ServiceProxy("/mujoco/reset", Trigger)
+        reset_service(TriggerRequest())
+    except rospy.ServiceException as error:
+        print(f"Service call failed: {error}")
+
+
+if __name__ == "__main__":
+    rospy.init_node("spawn_milk")
+    spawn_box()
+    rospy.sleep(1)
+    reset_simulation()
